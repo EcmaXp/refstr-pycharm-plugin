3a03b0bc7c7163b8b7cde17eccb3e39e55ad9b42
GitHub Action <action@github.com>
2021-01-27 13:15:35 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ecmaxp/refstrpycharmplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ecmaxp.refstrpycharmplugin.services
+
+import com.github.ecmaxp.refstrpycharmplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/ecmaxp/refstrpycharmplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ecmaxp/refstrpycharmplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ecmaxp.refstrpycharmplugin.services
 
+import com.github.ecmaxp.refstrpycharmplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
